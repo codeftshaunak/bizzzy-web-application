838bbd7f97c6d4147ef94f65429c72d70bb22324
tonmoydeb404 <tonmoydeb404@gmail.com>
2023-11-08 00:47:13 +0600

    implemented react-hook-form with basic validation

--- src/Pages/JobPost/index.jsx
+++ src/Pages/JobPost/index.jsx
@@ -1,25 +1,48 @@
-import React, { useState } from 'react'
-import FirstStep from '../../Components/JobCreate/FirstStep';
-import HomeLayout from '../../Layouts/HomeLayout';
-import SecondStep from '../../Components/JobCreate/SecondStep';
-import FinalStep from '../../Components/JobCreate/FinalStep';
-import Complete from '../../Components/JobCreate/Completed';
+import { zodResolver } from "@hookform/resolvers/zod";
+import React, { useState } from "react";
+import { FormProvider, useForm } from "react-hook-form";
+import z from "zod";
+import Complete from "../../Components/JobCreate/Completed";
+import FinalStep from "../../Components/JobCreate/FinalStep";
+import FirstStep from "../../Components/JobCreate/FirstStep";
+import SecondStep from "../../Components/JobCreate/SecondStep";
+import HomeLayout from "../../Layouts/HomeLayout";
+
+const schema = z.object({
+  title: z.string(),
+  description: z.string(),
+  tags: z.string(),
+  type: z.string(),
+  amount: z.number(),
+  experience: z.string(),
+  duration: z.string(),
+});
 
 const JobPost = () => {
-    const [step, setStep] = useState(1)
-    return (
-        <HomeLayout displaydir="row">
-            {
-                step === 1 && <FirstStep setStep={setStep} />
-            }{
-                step === 2 && <SecondStep setStep={setStep} />
-            }{
-                step === 3 && <FinalStep setStep={setStep} />
-            }{
-                step === 4 && <Complete setStep={setStep} />
-            }
-        </HomeLayout>
-    )
-}
+  const [step, setStep] = useState(1);
+
+  const methods = useForm({
+    resolver: zodResolver(schema),
+  });
+  const { handleSubmit } = methods;
+
+  const onSubmit = (values) => {
+    console.log(values);
+    setStep(4);
+  };
+
+  return (
+    <HomeLayout displaydir="row">
+      <FormProvider {...methods}>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          {step === 1 && <FirstStep setStep={setStep} />}
+          {step === 2 && <SecondStep setStep={setStep} />}
+          {step === 3 && <FinalStep setStep={setStep} />}
+          {step === 4 && <Complete setStep={setStep} />}
+        </form>
+      </FormProvider>
+    </HomeLayout>
+  );
+};
 
 export default JobPost;
