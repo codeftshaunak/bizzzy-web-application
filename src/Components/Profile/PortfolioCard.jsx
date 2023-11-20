fdfc55d5610d95a8684c2c6bc6ab2345842e25f5
joydip62 <joydipbanik6@gmail.com>
2023-11-20 10:55:23 +0530

    education data fetch and create insert function for education and portfolio

--- src/Components/Profile/PortfolioCard.jsx
+++ src/Components/Profile/PortfolioCard.jsx
@@ -1,13 +1,17 @@
 import React from "react";
 import portfoliobg from "../../assets/portfoliobg.png";
-const PortfolioCard = () => {
+const PortfolioCard = ({ portfolio }) => {
+  const { project_name, project_description, technologies } = portfolio;
   return (
     <div className="flex flex-col gap-[12px]">
       <div
         className=" h-[135px] w-[202px] rounded-[12px]"
         style={{ background: `url(${portfoliobg})`, backgroundSize: "contain" }}
       ></div>
-      <p className="text-[14px] text-[#374151] font-[500]">Project #1</p>
+      <p className="text-[14px] text-[#374151] font-[500]">
+        {project_name}
+      </p>
+      
     </div>
   );
 };
