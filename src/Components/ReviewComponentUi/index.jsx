b57f366aa073228501af69b69bd965ef744ffa9e
Ashim Rudra Paul <codewithashim@gmail.com>
2023-12-12 16:56:31 +0600

    revew are done

--- src/Components/ReviewComponentUi/index.jsx
+++ src/Components/ReviewComponentUi/index.jsx
@@ -1,12 +1,5 @@
-import {
-  Box,
-  HStack,
-  Heading,
-  Select,
-  Text,
-  Textarea,
-  VStack,
-} from "@chakra-ui/react";
+import { Box, HStack, Heading, Select, Text, Textarea } from "@chakra-ui/react";
+import { FaStar } from "react-icons/fa";
 
 const ReviewComponentUi = () => {
   const ratingNumber = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
@@ -105,6 +98,145 @@ const ReviewComponentUi = () => {
               <option value="option2">Option 2</option>
               <option value="option3">Option 3</option>
             </Select>
+          </Box>
+        </Box>
+      </Box>
+      <Box
+        marginTop={"10"}
+        className="w-full border rounded-lg shadow px-6 py-6"
+      >
+        <Box>
+          <Heading>Public feedback</Heading>
+          <Text fontSize="2xl" marginTop={"2"}>
+            This is your opportunity to share feedback on Steven that you{" "}
+            {"don't"} want posted publicly.{"We'll"} use it to improve the user
+            experience, but we {"won't"} share it Steven.{" "}
+            <Box as="a" href="#" fontWeight="bold" style={{ color: "#22c55e" }}>
+              Learn more
+            </Box>
+          </Text>
+        </Box>
+
+        <Box marginTop={"10"}>
+          <Text fontSize="2xl" fontWeight="bold">
+            Feedback to client
+          </Text>
+          <Box marginTop={"3"} className="flex flex-col gap-8">
+            <Box className="flex items-center gap-6">
+              <Box className="flex items-center gap-2">
+                <FaStar className="text-[2rem] cursor-pointer text-[#d1d5db]" />
+                <FaStar className="text-[2rem] cursor-pointer text-[#d1d5db]" />
+                <FaStar className="text-[2rem] cursor-pointer text-[#d1d5db]" />
+                <FaStar className="text-[2rem] cursor-pointer text-[#d1d5db]" />
+                <FaStar className="text-[2rem] cursor-pointer text-[#d1d5db]" />
+              </Box>
+              <Text fontSize={"xl"}>Skills</Text>
+            </Box>
+
+            <Box className="flex items-center gap-6">
+              <Box className="flex items-center gap-2">
+                <FaStar className="text-[2rem] text-[#d1d5db] cursor-pointer" />
+                <FaStar className="text-[2rem] text-[#d1d5db] cursor-pointer" />
+                <FaStar className="text-[2rem] text-[#d1d5db] cursor-pointer" />
+                <FaStar className="text-[2rem] text-[#d1d5db] cursor-pointer" />
+                <FaStar className="text-[2rem] text-[#d1d5db] cursor-pointer" />
+              </Box>
+              <Text fontSize={"xl"}>Quality of Requierments</Text>
+            </Box>
+
+            <Box className="flex items-center gap-6">
+              <Box className="flex items-center gap-2">
+                <FaStar className="text-[2rem] text-[#d1d5db] cursor-pointer" />
+                <FaStar className="text-[2rem] text-[#d1d5db] cursor-pointer" />
+                <FaStar className="text-[2rem] text-[#d1d5db] cursor-pointer" />
+                <FaStar className="text-[2rem] text-[#d1d5db] cursor-pointer" />
+                <FaStar className="text-[2rem] text-[#d1d5db] cursor-pointer" />
+              </Box>
+              <Text fontSize={"xl"}>Avavilability</Text>
+            </Box>
+
+            <Box className="flex items-center gap-6">
+              <Box className="flex items-center gap-2">
+                <FaStar className="text-[2rem] text-[#d1d5db] cursor-pointer" />
+                <FaStar className="text-[2rem] text-[#d1d5db] cursor-pointer" />
+                <FaStar className="text-[2rem] text-[#d1d5db] cursor-pointer" />
+                <FaStar className="text-[2rem] text-[#d1d5db] cursor-pointer" />
+                <FaStar className="text-[2rem] text-[#d1d5db] cursor-pointer" />
+              </Box>
+              <Text fontSize={"xl"}>Set Responable Deadlines</Text>
+            </Box>
+
+            <Box className="flex items-center gap-6">
+              <Box className="flex items-center gap-2">
+                <FaStar className="text-[2rem] text-[#d1d5db] cursor-pointer" />
+                <FaStar className="text-[2rem] text-[#d1d5db] cursor-pointer" />
+                <FaStar className="text-[2rem] text-[#d1d5db] cursor-pointer" />
+                <FaStar className="text-[2rem] text-[#d1d5db] cursor-pointer" />
+                <FaStar className="text-[2rem] text-[#d1d5db] cursor-pointer" />
+              </Box>
+              <Text fontSize={"xl"}>Communication</Text>
+            </Box>
+
+            <Box className="flex items-center gap-6">
+              <Box className="flex items-center gap-2">
+                <FaStar className="text-[2rem] text-[#d1d5db] cursor-pointer" />
+                <FaStar className="text-[2rem] text-[#d1d5db] cursor-pointer" />
+                <FaStar className="text-[2rem] text-[#d1d5db] cursor-pointer" />
+                <FaStar className="text-[2rem] text-[#d1d5db] cursor-pointer" />
+                <FaStar className="text-[2rem] text-[#d1d5db] cursor-pointer" />
+              </Box>
+              <Text fontSize={"xl"}>Cooperation</Text>
+            </Box>
+          </Box>
+        </Box>
+        <Text fontWeight={"bold"} fontSize={"3xl"} marginTop={"4"}>
+          Total Score:0.00
+        </Text>
+
+        <Box marginTop={"10"}>
+          <Text fontSize="2xl" fontWeight="bold">
+            Share your experience with this client to the Upwork community
+          </Text>
+
+          <Box marginTop={"3"}>
+            <Textarea
+              padding={"4"}
+              height={"40"}
+              fontSize={"xl"}
+              placeholder="Your comments will be shared publicly"
+            />
+          </Box>
+        </Box>
+
+        <Box marginTop={"10"}>
+          <Text fontSize="2xl" marginTop={"2"} fontWeight="bold">
+            See and
+            <Box as="a" href="#" marginLeft={"2"} style={{ color: "#22c55e" }}>
+              example of appropiate feedback
+            </Box>
+          </Text>
+        </Box>
+      </Box>
+
+      <Box
+        marginTop={"10"}
+        className="w-full border rounded-lg shadow px-6 py-6"
+      >
+        <Box>
+          <Text fontSize="2xl">
+            Eending this contract will permanently lock the Work Diary for this
+            project. {"We'll"} let your client know the job is done and send you
+            a final statement for any unpaid work
+          </Text>
+        </Box>
+        <Box marginTop={"4"}>
+          <Box className="flex items-center gap-6">
+            <Text fontSize="3xl" fontWeight={"bold"} className="text-[#22c55e]">
+              Cancel
+            </Text>
+            <button className="my-6 font-semibold text-[1.4rem] rounded-full px-4 py-2 bg-green-500 text-white">
+              Submit Review
+            </button>
           </Box>
         </Box>
       </Box>
